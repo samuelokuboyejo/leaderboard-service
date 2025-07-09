f88803b4070f2fea9947beca258f1ecf9d0af333
samuelokuboyejo <sammyokuboyejo@gmail.com>
2025-07-09 14:06:27 +0100

    Initial Spring Boot project

---
+++ src/main/java/com/leaderboardservice/LeaderboardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.leaderboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderboardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderboardServiceApplication.class, args);
+	}
+
+}
